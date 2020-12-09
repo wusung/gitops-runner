837943bf2be13767f14508701568e5cea4057909
wusung <twkoci@gmail.com>
2020-12-09 23:17:29 +0800

    add systemd service content

--- install.sh
+++ install.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env bash
 
 WORK_DIR=${HOME}/.gitlab-deploy-${RANDOM}
+SYSTEMD_SERVICE="/etc/systemd/system/gitlab-deploy.service"
+
 ## Clone the repo
 git clone https://github.com/wusung/gitlab-deploy.git ${WORK_DIR} || { echo >&2 "Clone failed with $?"; exit 1; }
 pushd ${WORK_DIR}>/dev/null
 npm install
 popd >/dev/null
 
-SYSTEMD_SERVICE="/etc/systemd/system/gitlab-deploy.service"
-
 if [ ! -f ${SYSTEMD_SERVICE} ]; then
     cat >${SYSTEMD_SERVICE} <<EOF
-${WORK_DIR}
+[Unit]
+Description=Gitlab Ddeploy service
+After=network.target
+StartLimitIntervalSec=0
+[Service]
+Type=simple
+Restart=always
+RestartSec=1
+User=root
+ExecStart=/usr/bin/env node ${WORK_DIR}/server.js
+
+[Install]
+WantedBy=multi-user.target
+
 EOF
     echo "${SYSTEMD_SERVICE} created."
 fi
