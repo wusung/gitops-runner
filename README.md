4085a48792916b84bcfa4a0768757269db5d9ae2
wusung <twkoci@gmail.com>
2020-12-09 19:53:50 +0800

    add systemd service

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # Gitlab deploy
 
+## How to install
+
+```
+curl https://raw.githubusercontent.com/wusung/gitlab-deploy/master/install.sh | bash -
+```
+
 ## Build
 
-Run `yarn buid` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+Run `yarn buid` to build the project. The build artifacts will be stored in the `dist/` directory.
 
-## Running unit tests
+## Start the service with systemd
 
 Run `sudo service gitlab-deploy` to start the service.
--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
-TMP_DIR=$HOME/.gitlab-deploy-$RANDOM
+WORK_DIR=${HOME}/.gitlab-deploy-${RANDOM}
 ## Clone the repo
-git clone https://gitlab.com/wusung/gitlab-deploy $TMP_DIR || { echo >&2 "Clone failed with $?"; exit 1; }
-pushd $TMP_DIR
-yarn
+git clone https://github.com/wusung/gitlab-deploy.git ${WORK_DIR} || { echo >&2 "Clone failed with $?"; exit 1; }
+pushd ${WORK_DIR}>/dev/null
+npm install
+popd >/dev/null
 
-popd
+SYSTEMD_SERVICE="/etc/systemd/system/gitlab-deploy.service"
+
+if [ ! -f ${SYSTEMD_SERVICE} ]; then
+    cat >${SYSTEMD_SERVICE} <<EOF
+${WORK_DIR}
+EOF
+    echo "${SYSTEMD_SERVICE} created."
+fi
