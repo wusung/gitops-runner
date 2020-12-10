843b3742876d067d9bbcae7154bdca29bc2c0f75
wusung <twkoci@gmail.com>
2020-12-10 11:27:34 +0800

    add eslinit

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-const crypto = require('crypto')
-const fs = require('fs')
-const path = require('path')
-const util = require('util')
-const { v4: uuid } = require('uuid')
-const { pipeline } = require('stream')
-const pump = util.promisify(pipeline)
-const bearerAuthPlugin = require('fastify-bearer-auth')
+const crypto = require('crypto');
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const { v4: uuid } = require('uuid');
+const { pipeline } = require('stream');
+
+const pump = util.promisify(pipeline);
+const bearerAuthPlugin = require('fastify-bearer-auth');
 const fastify = require('fastify')({
-  logger: false
-})
-
-const KEY_PATH = resolveHome('~/.gitlab-deploy/gitlab-deploy.key')
-const DEPLOY_PATH = resolveHome('~/.gitlab-deploy')
-
-let encrypedKey;
+  logger: false,
+});
 
 function resolveHome(filepath) {
   if (filepath[0] === '~') {
@@ -22,59 +18,60 @@ function resolveHome(filepath) {
   return filepath;
 }
 
+const KEY_PATH = resolveHome('~/.gitlab-deploy/gitlab-deploy.key');
+const DEPLOY_PATH = resolveHome('~/.gitlab-deploy');
+
 function ensurePath(pathname) {
   if (!fs.existsSync(resolveHome(pathname))) {
-    fs.mkdirSync(resolveHome(pathname), { recursive: true })
+    fs.mkdirSync(resolveHome(pathname), { recursive: true });
   }
-};
+}
 
-function createKey() { 
-  ensurePath(path.dirname(KEY_PATH))
+function createKey() {
+  ensurePath(path.dirname(KEY_PATH));
 
   if (!fs.existsSync(KEY_PATH)) {
-    let key = uuid();
+    const key = uuid();
 
     fs.writeFileSync(KEY_PATH, crypto.createHash('sha256').update(key).digest('base64'));
-    console.log(`Please write down the key. You can delete ${KEY_PATH} if you forget the key.`)
-    console.log(`key = ${key}`)
+    console.log(`Please write down the key. You can delete ${KEY_PATH} if you forget the key.`);
+    console.log(`key = ${key}`);
   }
 }
 
 function createHash(str) {
-  return crypto.createHash('sha256').update(str).digest('base64')
+  return crypto.createHash('sha256').update(str).digest('base64');
 }
 
 function getEncrypedKey() {
-  return fs.readFileSync(KEY_PATH, 'utf-8').trim()
+  return fs.readFileSync(KEY_PATH, 'utf-8').trim();
 }
 
-createKey()
+createKey();
 
 fastify.register(bearerAuthPlugin, {
-  auth: (key, req) => {
-    return new String(createHash(key)) == getEncrypedKey()
-  }
+  auth: (key, req) => new String(createHash(key)) == getEncrypedKey(),
 });
-fastify.register(require('fastify-multipart'))
-fastify.post('/deploy', async (req, reply) => {  
+fastify.register(require('fastify-multipart'));
 
-  ensurePath(path.dirname(DEPLOY_PATH))
+fastify.post('/deploy', async (req, reply) => {
+  ensurePath(path.dirname(DEPLOY_PATH));
 
-  const options = { limits: { fileSize: 200*1000*1000 } }
-  const data = await req.file(options)
-  const target = path.join(DEPLOY_PATH, data.filename)
+  const options = { limits: { fileSize: 200 * 1000 * 1000 } };
+  const data = await req.file(options);
+  const target = path.join(DEPLOY_PATH, data.filename);
 
-  console.log(`deploying to ${target}`)
-  await pump(data.file, fs.createWriteStream(target))
-  console.log(`deployed to ${target}`)
+  console.log(`deploying to ${target}`);
+  await pump(data.file, fs.createWriteStream(target));
+  console.log(`deployed to ${target}`);
   reply.send({
     name: data.filename,
     mimetype: data.mimetype,
-  })
-})
+  });
+});
 
 // Run the server!
 fastify.listen(3000, (err, address) => {
-  if (err) throw err
-  fastify.log.info(`server listening on ${address}`)
-})
+  if (err) throw err;
+  fastify.log.info(`server listening on ${address}`);
+});
