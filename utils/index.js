fd6a64a779e2ef89221546d2c8a563ee4ffc298d
wusung <twkoci@gmail.com>
2020-12-14 09:08:47 +0800

    format codes with 2 spaces

--- utils/index.js
+++ utils/index.js
@@ -4,47 +4,47 @@ const crypto = require('crypto');
 const { v4: uuid } = require('uuid');
 
 exports.formatDate = (date) => {
-    const d = new Date(date);
-    let month = '' + (d.getMonth() + 1);
-    let day = '' + d.getDate();
-    let year = '' + d.getFullYear();
-    let hours = '' + d.getHours();
-    let minutes = '' + d.getMinutes();
-    let seconds = '' + d.getSeconds();
-    return [
-        year.padStart(2, '0'),
-        month.padStart(2, '0'),
-        day.padStart(2, '0'),
-        hours.padStart(2, '0'),
-        minutes.padStart(2, '0'),
-        seconds.padStart(2, '0')].join('');
+  const d = new Date(date);
+  let month = '' + (d.getMonth() + 1);
+  let day = '' + d.getDate();
+  let year = '' + d.getFullYear();
+  let hours = '' + d.getHours();
+  let minutes = '' + d.getMinutes();
+  let seconds = '' + d.getSeconds();
+  return [
+    year.padStart(2, '0'),
+    month.padStart(2, '0'),
+    day.padStart(2, '0'),
+    hours.padStart(2, '0'),
+    minutes.padStart(2, '0'),
+    seconds.padStart(2, '0')].join('');
 }
 
 exports.ensurePath = (pathname) => {
-    if (!fs.existsSync(exports.resolveHome(pathname))) {
-        fs.mkdirSync(exports.resolveHome(pathname), { recursive: true });
-    }
+  if (!fs.existsSync(exports.resolveHome(pathname))) {
+    fs.mkdirSync(exports.resolveHome(pathname), { recursive: true });
+  }
 }
 
 exports.resolveHome = (filepath) => {
-    if (filepath[0] === '~') {
-        return path.join(process.env.HOME, filepath.slice(1));
-    }
-    return filepath;
+  if (filepath[0] === '~') {
+    return path.join(process.env.HOME, filepath.slice(1));
+  }
+  return filepath;
 }
 
 exports.createKey = (keyPath) => {
-    exports.ensurePath(path.dirname(keyPath));
+  exports.ensurePath(path.dirname(keyPath));
 
-    if (!fs.existsSync(keyPath)) {
-        const key = uuid();
+  if (!fs.existsSync(keyPath)) {
+    const key = uuid();
 
-        fs.writeFileSync(keyPath, crypto.createHash('sha256').update(key).digest('base64'));
-        console.log(`Please write down the key. You can delete ${keyPath} if you forget the key.`);
-        console.log(`key = ${key}`);
-    }
+    fs.writeFileSync(keyPath, crypto.createHash('sha256').update(key).digest('base64'));
+    console.log(`Please write down the key. You can delete ${keyPath} if you forget the key.`);
+    console.log(`key = ${key}`);
+  }
 }
 
 exports.createHash = (str) => {
-    return crypto.createHash('sha256').update(str).digest('base64');
+  return crypto.createHash('sha256').update(str).digest('base64');
 }
