61b6536f9b244ef4757ef0b560ff1e701189d191
wusung <twkoci@gmail.com>
2022-01-15 00:58:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -52,7 +52,6 @@ fastify.get('/start', async (req, reply) => {
   (async() => {
     console.log('start')
     try {
-    } catch (e) {
       console.log(await shellExecSync(`ls -l`))
       console.log(await shellExecSync(`cd ${APP_PATH} && /usr/bin/git reset --hard HEAD`))
       await shellExecSync(`cd ${APP_PATH} && /usr/bin/git pull origin feature/202201 --rebase`)
@@ -62,6 +61,8 @@ fastify.get('/start', async (req, reply) => {
       await shellExecSync(`cd ${APP_PATH} && docker-compose rm -f`)
       await shellExecSync(`cd ${APP_PATH} && docker-compose up -d`)
       console.log('end')      
+    } catch (e) {
+      console.log(e)
     }
   })()
   reply.send({
